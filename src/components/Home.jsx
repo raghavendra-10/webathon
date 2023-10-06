cfeacac1ff8ae012e435633ec87b9b24d12ffdb9
VATTIKUTI RAGHAVENDRA <vattikutiraghavendra3@gmail.com>
2023-10-06 15:29:56 +0530

    animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
 
-import Lottie from "react-lottie";
-import gif1 from "./gif1.json"
 
 const Home = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true, 
-    animationData: gif1,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  };
+
 
   return (
     <div>
       <Navbar />
       <div className="bg-cover bg-center w-full flex justify-center  min-h-screen">
-      <Lottie options={defaultOptions} width={1000}/>
+
        
       </div>
     </div>
