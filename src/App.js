8f050dc1ab7dfbd9720af185e3264dfbe2cf1bbc
Paladugu Sri Teja <amithapaladugu6@gmail.com>
2023-10-06 12:24:52 +0530

    pulling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CardComponent from './components/Card';
+
 
 function App() {
   return (
-    <div className="">
-  
+    <>
+    <div className="App">
+      <header className="flex justify-between items-center p-4 bg-blue-500">
+        <div className="flex space-x-4">
+          <img src="/path-to-image1.jpg" alt="Image1" className="w-16 h-16"/>
+          <img src="/path-to-image2.jpg" alt="Image2" className="w-16 h-16"/>
+        </div>
+        <div className="flex space-x-4">
+          <button className="text-white">Home</button>
+          <button className="text-white">Login</button>
+          <button className="text-white">Contact Us</button>
+        </div>
+      </header>
+      
+      <main className="p-10">
+        <section className="text-center my-10">
+          <h1 className="text-5xl font-bold">XYZ College Placement Section</h1>
+        </section>
+
+        <section className="flex flex-inline justify-around">
+            <CardComponent />
+        </section>
+      </main>
     </div>
+  </>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Card.css';
+
+function CardComponent() {
+  const cards = [
+    {
+      link: "https://www.mythrillfiction.com/the-dark-rider",
+      coverImage: "https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg",
+      titleImage: "https://ggayane.github.io/css-experiments/cards/dark_rider-title.png",
+      characterImage: "https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"
+    },
+    {
+      link: "https://www.mythrillfiction.com/force-mage",
+      coverImage: "https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg",
+      titleImage: "https://ggayane.github.io/css-experiments/cards/force_mage-title.png",
+      characterImage: "https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"
+    }
+  ];
+
+  return (
+    <div>
+      {cards.map((card, index) => (
+        <a key={index} href={card.link} alt="Mythrill" target="_blank" rel="noopener noreferrer">
+          <div className="card">
+            <div className="wrapper">
+              <img src={card.coverImage} alt="Cover" className="cover-image" />
+            </div>
+            <img src={card.titleImage} alt="Title" className="title" />
+            <img src={card.characterImage} alt="Character" className="character" />
+          </div>
+        </a>
+      ))}
+    </div>
+  );
+}
+
+export default CardComponent;
