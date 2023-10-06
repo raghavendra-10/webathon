7e312887503da55192a15b9d8797d2659f8eb072
VATTIKUTI RAGHAVENDRA <vattikutiraghavendra3@gmail.com>
2023-10-06 15:32:06 +0530

    animati

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
-
+import Logo from "../assests/newlogo.png";
 
 const Home = () => {
-
-
   return (
     <div>
       <Navbar />
-      <div className="bg-cover bg-center w-full flex justify-center  min-h-screen">
-
+      <div className="bg-cover bg-center w-full flex justify-center  min-h-screen"style={{ backgroundImage: `url(${Logo})` }}>
+        
        
       </div>
     </div>
