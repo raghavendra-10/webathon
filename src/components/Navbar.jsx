59b4a20d50a342db3e789a26435ef562090269af
Paladugu Sri Teja <amithapaladugu6@gmail.com>
2023-10-06 17:35:43 +0530

    Added Sliding Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assests/newlogo.png"; 
+import Human from "../assests/download.png"; 
 
 const Navbar = () => {
   return (
     <div className="bg-blue-500 py-4">
       <nav className="container mx-auto flex justify-between items-center">
         <div className="flex px-2 items-center">
-          <img src={Logo} alt="Divulge Logo" className="h-8  rounded-full w-8 mr-2" />
-          <h1 className="text-white text-lg sm:text-3xl font-semibold">DIVULGE</h1>
+          <img src={Human} alt="Divulge Logo" className="h-8  rounded-full w-8 mr-2" />
+          <h1 className="text-white text-lg sm:text-3xl font-semibold">Placement-Portal</h1>
         </div>
         <ul className="flex gap-2 sm:text-xl sm:space-x-4 over">
           
