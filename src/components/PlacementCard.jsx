865b5ec5361de451a4dbb9ccf3077fe991f504b4
Paladugu Sri Teja <amithapaladugu6@gmail.com>
2023-10-07 00:39:56 +0530

    data

--- src/components/PlacementCard.jsx
+++ src/components/PlacementCard.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-// import './PlacementCard.css';
 
 const PlacementCard = ({ placement }) => {
   return (
-    <div className="border border-gray-200 p-4 rounded-lg mb-4 shadow-md">
+    <div className="border border-gray-200 p-4 rounded-lg mb-4 shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl bg-gradient-to-r from-blue-200 to-black">
       <div className="flex flex-col items-center mb-4">
-      <img
+        <img
           src={placement.profilePhotoURL}
           alt="profile photo"
           className="w-40 h-40 object-cover rounded-full mb-2"
         />
-        <h2 className="text-xl font-semibold">{placement.registrationNumber}</h2>
-        <h2 className="text-xl font-semibold">{placement.username}</h2>
+        <h2 className="text-xl font-semibold text-navy-900">{placement.registrationNumber}</h2>
+        <h2 className="text-xl font-semibold text-navy-900">{placement.username}</h2>
       </div>
-      <p className="text-gray-800 text-center mt-2">Batch: {placement.batch}</p>
-      <p className="text-gray-800 text-center mt-2">Branch: {placement.branch}</p>
-      <p className="text-gray-800 text-center mt-2">Company: {placement.companyName}</p>
-      <p className="text-gray-800 text-center mt-2">Package Offered: {placement.packageOffered}</p>
+      <p className="text-navy-900 text-center mt-2">Batch: {placement.batch}</p>
+      <p className="text-navy-900 text-center mt-2">Branch: {placement.branch}</p>
+      <p className="text-navy-900 text-center mt-2">Company: {placement.companyName}</p>
+      <p className="text-navy-900 text-center mt-2">Package Offered: {placement.packageOffered}</p>
       {/* Add more placement fields as needed */}
     </div>
   );
 };
+
 export default PlacementCard;
--- src/components/Placements.jsx
+++ src/components/Placements.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebaseConfig"; // Import Firestore methods
 import PlacementCard from "./PlacementCard";
-// import './Placements.css';
 
-const Placements = () => {
+const Placements = ({ placement }) => {
   const [placements, setPlacements] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const fetchPlacements = async () => {
@@ -23,12 +24,33 @@ const Placements = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-2xl font-semibold mb-4">Placements</h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {placements.map((placement) => (
-          <PlacementCard key={placement.uid} placement={placement} />
-        ))}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-200 to-black p-4">
+      <div className="flex items-center mb-6 space-x-4">
+        {/* Link to the home page */}
+        <Link to="/dashboard">
+          <button className="bg-green-400 px-2 py-1 rounded hover:text-white">Home</button>
+        </Link>
+        <h1 className="text-4xl font-bold text-navy-900">Placements</h1>
+        <div className="relative">
+          <input 
+            type="text" 
+            placeholder="Search..." 
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="pl-8 pr-4 py-2 rounded-full bg-navy-900 bg-opacity-20 text-white focus:outline-none focus:bg-opacity-40 transition duration-300 ease-in-out"
+          />
+          <svg className="w-4 h-4 text-white absolute top-1/2 left-2 transform -translate-y-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-6a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-5xl">
+        {placements
+          .filter((placement) => 
+            placement.username.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((placement) => (
+            <PlacementCard key={placement.uid} placement={placement} />
+          ))}
       </div>
     </div>
   );
